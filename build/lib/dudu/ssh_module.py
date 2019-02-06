7553606cbc1958a6eb80d8221479cec32e2d7839
Debian <ec2-user@localhost.localdomain>
2019-02-06 12:25:11 +0000

    remove build files

--- build/lib/dudu/ssh_module.py
+++
@@ -1,9 +0,0 @@
-from dudu import listinstances_module
-import subprocess
-import os
-
-def remote_ssh(number, user):
-    instance    =   list(listinstances_module.instances)[int(number)]
-    key_path    =   "{}/.ssh/{}.pem".format(os.path.expanduser("~"),instance.key_name)
-    command     =   "ssh -i {} {}@{}".format(key_path, user, instance.public_ip_address)
-    subprocess.call(command, shell=True)
